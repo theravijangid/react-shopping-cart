dac936b5b37de111d566e607d01441a3600a404b
Ravikiran Jangid <ravijangir993@gmail.com>
2023-09-28 20:22:19 +0530

    Add files via upload

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Spinner = () => {
+  return (
+    <div className='spinner  w-[100vw] h-[100vh] flex justify-center items-center font-bold text-3xl overflow-hidden'>Loading</div>
+  )
+}
+
+export default Spinner
